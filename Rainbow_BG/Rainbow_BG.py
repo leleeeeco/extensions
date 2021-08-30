cef2a1a9ba0120a9eacecec71289619e2976fc02
Laande <xerixexe@gmail.com>
2021-08-30 14:26:46 +0200

    Change bubble

--- Rainbow_BG/Rainbow_BG.py
+++ Rainbow_BG/Rainbow_BG.py
@@ -39,37 +39,37 @@ def speech(p):
 
     if text == SET_ID:
         p.is_blocked = True
-        ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Modify the background to save the id"}{i:0}{i:34}{i:0}{i:0}')
+        ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Modify the background to save the id"}{i:0}{i:30}{i:0}{i:0}')
         set_id = True
 
     elif text == START:
         p.is_blocked = True
         if on:
-            return ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Already on"}{i:0}{i:34}{i:0}{i:0}')
+            return ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Already on"}{i:0}{i:30}{i:0}{i:0}')
         if idd:
             on = True
             thread = threading.Thread(target=main)
             thread.start()
-            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Rainbow on"}{i:0}{i:34}{i:0}{i:0}')
+            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Rainbow on"}{i:0}{i:30}{i:0}{i:0}')
         else:
-            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Id not found"}{i:0}{i:34}{i:0}{i:0}')
-            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Please use `'+SET_ID+'`"}{i:0}{i:34}{i:0}{i:0}')
+            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Id not found"}{i:0}{i:30}{i:0}{i:0}')
+            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Please use `'+SET_ID+'`"}{i:0}{i:30}{i:0}{i:0}')
 
     elif text == STOP:
         p.is_blocked = True
         if not on:
-            return ext.send_to_client('{in:'+SPEECH+'}{i:0}{s:"Already off"}{i:0}{i:34}{i:0}{i:0}')
+            return ext.send_to_client('{in:'+SPEECH+'}{i:0}{s:"Already off"}{i:0}{i:30}{i:0}{i:0}')
         on = False
-        ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Rainbow off"}{i:0}{i:34}{i:0}{i:0}')
+        ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Rainbow off"}{i:0}{i:30}{i:0}{i:0}')
 
     elif text.startswith(SET_DELAY):
         p.is_blocked = True
         try:
             delay = float(text[(len(SET_DELAY)):])
             cool_down = delay
-            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Interval set to: '+str(cool_down)+'"}{i:0}{i:34}{i:0}{i:0}')
+            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Interval set to: '+str(cool_down)+'"}{i:0}{i:30}{i:0}{i:0}')
         except ValueError:
-            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Only number available"}{i:0}{i:34}{i:0}{i:0}')
+            ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Only number available"}{i:0}{i:30}{i:0}{i:0}')
 
 
 def main():
@@ -86,7 +86,7 @@ def save_id(p):
     if set_id:
         furni_id, _, _, _ = p.packet.read("iiii")
         idd = str(furni_id)
-        ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Id save ('+idd+')"}{i:0}{i:34}{i:0}{i:0}')
+        ext.send_to_client('{in:'+SPEECH+'}{i:123456789}{s:"Id save ('+idd+')"}{i:0}{i:30}{i:0}{i:0}')
         set_id = False
 
 
